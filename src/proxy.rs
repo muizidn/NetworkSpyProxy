3a6e4cd18db0521208f439b38f035164737c9014
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2024-07-27 19:31:26 +0700

    Create Proxy to be used by Client

---
+++ src/proxy.rs
@@ -0,0 +1,99 @@
+use bytes::Bytes;
+use hudsucker::{
+    certificate_authority::RcgenAuthority,
+    hyper::{Request, Response},
+    rcgen::{CertificateParams, KeyPair},
+    tokio_tungstenite::tungstenite::Message,
+    *,
+};
+use std::{net::SocketAddr, sync::{atomic::{AtomicBool, Ordering}, Arc}};
+use tokio::{runtime::{Builder, Runtime}, sync::Notify};
+use tracing::*;
+
+use crate::traffic::{TrafficInterceptor, TrafficListener};
+
+pub struct Proxy {
+    key_pair: &'static str,
+    ca_cert: &'static str,
+    port: u16,
+    notify: Arc<Notify>,
+    is_running: Arc<AtomicBool>,
+}
+
+impl Proxy {
+    pub fn new(key_pair: &'static str, ca_cert: &'static str, port: u16) -> Self {
+        Proxy {
+            key_pair,
+            ca_cert,
+            port,
+            notify: Arc::new(Notify::new()),
+            is_running: Arc::new(AtomicBool::new(true)),
+        }
+    }
+
+    async fn shutdown_signal(&self) {
+        self.notify.notified().await;
+    }
+
+    pub fn run_proxy(&mut self, listener: Arc<dyn TrafficListener + Send + Sync>) {
+        tracing_subscriber::fmt::init();
+
+        let runtime = Builder::new_current_thread()
+            .worker_threads(4)
+            .thread_name("network-spy-proxy")
+            .thread_stack_size(3 * 1024 * 1024)
+            .enable_io()
+            .enable_time()
+            .build()
+            .unwrap();
+
+        let key_pair = KeyPair::from_pem(self.key_pair).expect("Failed to parse private key");
+        let ca_cert = CertificateParams::from_ca_cert_pem(self.ca_cert)
+            .expect("Failed to parse CA certificate")
+            .self_signed(&key_pair)
+            .expect("Failed to sign CA certificate");
+
+        let ca = RcgenAuthority::new(key_pair, ca_cert, 1_000);
+
+        let traffic = TrafficInterceptor::new(listener);
+
+        let proxy = hudsucker::Proxy::builder()
+            .with_addr(SocketAddr::from(([127, 0, 0, 1], self.port)))
+            .with_rustls_client()
+            .with_ca(ca)
+            .with_http_handler(traffic.clone())
+            .with_websocket_handler(traffic.clone())
+            // FIXME: I don't know how to fix yet 🥹
+            // .with_graceful_shutdown(self.shutdown_signal())
+            .build();
+
+            runtime.block_on(async {
+                if let Err(e) = proxy.start().await {
+                    error!("{}", e);
+                }
+            });
+    }
+
+    pub fn stop_proxy(&mut self) {
+        self.is_running.store(false, Ordering::SeqCst);
+        self.notify.notify_one();
+    }
+}
+
+// struct MyTrafficListener;
+
+// impl TrafficListener for MyTrafficListener {
+//     fn request(&self, id: u64, request: Request<Bytes>) {
+//         println!("Received request with id {}: {:?}", id, request);
+//     }
+
+//     fn response(&self, id: u64, response: Response<Bytes>) {
+//         println!("Sending response with id {}: {:?}", id, response);
+//     }
+// }
+
+// fn main() {
+//     let proxy = Proxy::new("path/to/cert".to_string(), 3000);
+//     let listener = Arc::new(MyTrafficListener);
+//     proxy.run_proxy(listener);
+// }
---
+++ src/traffic.rs
@@ -0,0 +1,126 @@
+use core::{hash, str};
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use async_trait::async_trait;
+use hudsucker::{
+    hyper::{Request, Response},
+    tokio_tungstenite::tungstenite::Message,
+    *,
+};
+use bytes::Bytes;
+use http_body_util::{BodyExt, Full};
+
+use std::sync::Arc;
+
+pub trait TrafficListener: Sync + Send {
+    fn request(&self, id: u64, request: Request<Bytes>);
+    fn response(&self, id: u64, response: Response<Bytes>);
+}
+
+#[derive(Clone)]
+pub struct TrafficInterceptor {
+    listener: Arc<dyn TrafficListener>,
+}
+
+impl TrafficInterceptor {
+    pub fn new(listener: Arc<dyn TrafficListener>) -> Self {
+        TrafficInterceptor { listener }
+    }
+}
+struct RequestDuplicate {
+    origin: Request<Body>,
+    duplicate: Request<Bytes>
+}
+
+async fn duplicate_req(req: Request<Body>) -> RequestDuplicate {
+    let (parts, body) = req.into_parts();
+    
+    let old_uri = &parts.uri;
+    let old_method = &parts.method;
+    let old_headers = &parts.headers;
+    let old_ver = &parts.version;
+    let old_bytes = body.collect().await.unwrap().to_bytes();
+    let new_bytes = old_bytes.clone();
+
+    let mut req1 = Request::builder()
+        .uri(old_uri)
+        .method(old_method)
+        .version(*old_ver)
+        .body(old_bytes)
+        .unwrap();
+        for (k,v) in old_headers {
+            let k_c = k.clone();
+            let v_c = v.clone();
+            req1.headers_mut().append(&k_c, v_c);
+        }
+
+    let req = Request::from_parts(parts, Body::from(Full::new(new_bytes)));
+    
+    RequestDuplicate { origin: req, duplicate: req1 }
+}
+
+struct ResponseDuplicate {
+    origin: Response<Body>,
+    duplicate: Response<Bytes>
+}
+
+async fn duplicate_res(res: Response<Body>) -> ResponseDuplicate {
+    let (parts, body) = res.into_parts();
+    
+    let old_status = &parts.status.clone();
+    let old_headers = &parts.headers;
+    let old_ver = &parts.version;
+    let old_bytes = body.collect().await.unwrap().to_bytes();
+
+    let new_bytes = old_bytes.clone();
+
+    let mut res1 = Response::builder()
+        .status(old_status)
+        .version(*old_ver)
+        .body(old_bytes)
+        .unwrap();
+        for (k,v) in old_headers {
+            let k_c = k.clone();
+            let v_c = v.clone();
+            res1.headers_mut().append(&k_c, v_c);
+        }
+
+    let res = Response::from_parts(parts, Body::from(Full::new(new_bytes)));
+    
+    ResponseDuplicate { origin: res, duplicate: res1 }
+}
+
+// #[async_trait]
+impl HttpHandler for TrafficInterceptor {
+    async fn handle_request(&mut self, _ctx: &HttpContext, req: Request<Body> ) -> RequestOrResponse {
+        let d = duplicate_req(req).await;
+        let original_request = d.origin;
+        let duplicated_request = d.duplicate;
+
+        let mut hasher = DefaultHasher::new();
+        _ctx.hash(&mut hasher);
+        self.listener.request(hasher.finish(), duplicated_request);
+
+        RequestOrResponse::Request(original_request)
+    }
+
+    async fn handle_response(&mut self, _ctx: &HttpContext, res: Response<Body>) -> Response<Body> {
+        let d = duplicate_res(res).await;
+        let original_response = d.origin;
+        let duplicated_response = d.duplicate;
+
+        let mut hasher = DefaultHasher::new();
+        _ctx.hash(&mut hasher);
+        self.listener.response(hasher.finish(), duplicated_response);
+
+        original_response
+    }
+}
+
+// #[async_trait]
+impl WebSocketHandler for TrafficInterceptor {
+    async fn handle_message(&mut self, _ctx: &WebSocketContext, msg: Message) -> Option<Message> {
+        println!("{:?}", msg);
+        Some(msg)
+    }
+}
