b5b80be528673108f64607b18a38597ea57669d1
ikbalvrsystem <ikbal@vrisystem.com>
2026-03-16 15:18:32 +0700

    Make ip_addr.c cross platform

--- src/c/ip_addr.c
+++ src/c/ip_addr.c
@@ -1,31 +1,101 @@
-
-// https://www.sanfoundry.com/c-program-get-ip-address/
-
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <unistd.h>
-#include <arpa/inet.h>
- 
-char * get_ip_addr()
-{
-    int n;
-    struct ifreq ifr;
-    char array[] = "en1";
-    n = socket(AF_INET, SOCK_DGRAM, 0);
-    ifr.ifr_addr.sa_family = AF_INET;
-    strncpy(ifr.ifr_name , array , IFNAMSIZ - 1);
-    ioctl(n, SIOCGIFADDR, &ifr);
-    close(n);
-    return inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr);
+
+#ifdef _WIN32
+
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+char *get_ip_addr() {
+    IP_ADAPTER_ADDRESSES *addresses = NULL;
+    IP_ADAPTER_ADDRESSES *addr = NULL;
+    IP_ADAPTER_UNICAST_ADDRESS *unicast = NULL;
+
+    ULONG size = 15000;
+
+    addresses = (IP_ADAPTER_ADDRESSES *)malloc(size);
+    if (!addresses) return NULL;
+
+    if (GetAdaptersAddresses(AF_INET, 0, NULL, addresses, &size) != NO_ERROR) {
+        free(addresses);
+        return NULL;
+    }
+
+    for (addr = addresses; addr != NULL; addr = addr->Next) {
+        for (unicast = addr->FirstUnicastAddress; unicast != NULL; unicast = unicast->Next) {
+
+            SOCKADDR_IN *sa = (SOCKADDR_IN *)unicast->Address.lpSockaddr;
+
+            if (sa->sin_addr.S_un.S_addr == htonl(INADDR_LOOPBACK))
+                continue;
+
+            char *ip = malloc(INET_ADDRSTRLEN);
+            if (!ip) {
+                free(addresses);
+                return NULL;
+            }
+
+            inet_ntop(AF_INET, &(sa->sin_addr), ip, INET_ADDRSTRLEN);
+
+            free(addresses);
+            return ip;
+        }
+    }
+
+    free(addresses);
+    return NULL;
 }
 
-void free_ip_addr(char * ip) 
-{
-    free(ip);
-} 
+#else
+
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <net/if.h>
+
+char *get_ip_addr() {
+    struct ifaddrs *ifaddr = NULL;
+    struct ifaddrs *ifa = NULL;
+
+    if (getifaddrs(&ifaddr) == -1)
+        return NULL;
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+
+        if (!ifa->ifa_addr)
+            continue;
+
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+
+            struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+
+            if (ntohl(sa->sin_addr.s_addr) == INADDR_LOOPBACK)
+                continue;
+
+            char *ip = malloc(INET_ADDRSTRLEN);
+            if (!ip) {
+                freeifaddrs(ifaddr);
+                return NULL;
+            }
+
+            inet_ntop(AF_INET, &(sa->sin_addr), ip, INET_ADDRSTRLEN);
+
+            freeifaddrs(ifaddr);
+            return ip;
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return NULL;
+}
+
+#endif
+
+void free_ip_addr(char *ip) {
+    if (ip)
+        free(ip);
+}
